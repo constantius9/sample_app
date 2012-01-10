6a80344ccb4e18463c3d619cfb9eea7c49256d95
Michael Pankov <constantius9@gmail.com>
2012-01-10 19:05:38 +0400

    Added successfull Sign up handling

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -53,7 +53,8 @@ describe UsersController do
     describe "failure" do
 
       before(:each) do
-        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+        @attr = { :name => "", :email => "", 
+          :password => "", :password_confirmation => "" }
       end
 
       it "should not create a user" do
@@ -70,6 +71,30 @@ describe UsersController do
       it "should render the 'new' page" do
         post :create, :user => @attr
         response.should render_template('new')
+      end
+    end
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com", 
+          :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "should create a user" do
+        lambda do 
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to User profile page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should show welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
       end
     end
   end
