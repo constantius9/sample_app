9c5bdfd7fc9a4314e354418c81f6c78f74f8eb8a
Michael Pankov <constantius9@gmail.com>
2012-01-10 20:42:53 +0400

    Added sign-in failure handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid user/password combination"
+      @title = "Sign in"
+      render 'new'
+    else
+      # sign in the user and redirect
+    end
   end
 
   def destroy
