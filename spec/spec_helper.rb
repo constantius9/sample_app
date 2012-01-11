77e5540c563897334373d15b48fca5c8e04f60cb
Michael Pankov <constantius9@gmail.com>
2012-01-11 16:21:54 +0400

    Added sign-in-out, autotest ignore settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,31 +5,7 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  #ENV['RAILS_ENV'] ||= 'test'
-  #require File.expand_path("../../config/environment", __FILE__)
-  #require 'rspec/rails'
 
-  ## Requires supporting files with custom matchers and macros, etc,
-  ## in ./support/ and its subdirectories.
-  #Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
-  #RSpec.configure do |config|
-    ## == Mock Framework
-    ## 
-    ## If you prefer to use mocha, flexmock or RR, uncomment the approtiate line:
-    ##
-    ## config.mock_with :mocha
-    ## config.mock_with :flexmock
-    ## config.mock_with :rr
-    #config.mock_with :rspec
-
-    #config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-    ## If you are not using ActiveRecord, or you'd prefer not to run each of your
-    ## examples within a transaction, comment the following line or assign false
-    ## instead of true.
-    #config.use_transactional_fixtures = true
-  #end
 end
 
 Spork.each_run do
@@ -95,4 +71,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  def test_sign_in(user)
+    controller.sign_in(user)
+  end
 end
