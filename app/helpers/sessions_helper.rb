1418dc6efdcbba9edfe5b902e222826f62b10feb
Michael Pankov <constantius9@gmail.com>
2012-01-13 14:19:41 +0400

    Refactored to use session instead of cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:remember_token] = [user.id, user.salt]
     self.current_user = user
   end
 
@@ -26,7 +26,7 @@ module SessionsHelper
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+    session.delete(:remember_token)
     self.current_user = nil
   end
 
@@ -37,6 +37,6 @@ module SessionsHelper
     end
 
     def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
+      session[:remember_token] || [nil, nil]
     end
 end
