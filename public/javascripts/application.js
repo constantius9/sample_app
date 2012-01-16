d391d1e045cd5b3ea112ae29c39c9c1c4c5603ed
Michael Pankov <constantius9@gmail.com>
2012-01-16 14:35:18 +0400

    Added a counter of remaining symbols

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function countChars(field, counter, limit) {
+  if (field.value.length > limit)
+    field.value = field.value.substring(0, limit);
+  else
+    counter.innerText = (limit - field.value.length).toString();
+  return counter.innerText;
+}
